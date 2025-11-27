e3847901b27f7abe1a321353cfc5b854c684ace3
meachsenbroseth <153406625+meachsenbroseth@users.noreply.github.com>
2025-11-27 07:52:02 +0700

    fix run

--- run.py
+++ run.py
@@ -1,19 +1,6 @@
-# from app import create_app
-
-# app = create_app()
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-# run.py
 from app import create_app
-import os
 
-if __name__ == '__main__':
-    app = create_app()
-    
-    # Debug information
-    print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
-    print(f"Instance path: {app.instance_path}")
-    
+app = create_app()
+
+if __name__ == "__main__":
     app.run(debug=True)
